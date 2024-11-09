9a3efa2c86867763238db4019e853504648db5de
Bit D13 <bit@d13.co>
2024-11-10 00:32:56 +0100

    Home label change

--- src/components/Modules/Explorer/Home/Home.tsx
+++ src/components/Modules/Explorer/Home/Home.tsx
@@ -19,7 +19,7 @@ function Home(): JSX.Element {
         <div className={"home-container"}>
             <div className="home-body">
                 <div className="tag-line">
-                    Algo <span style={{color: primary}}>{network}</span> <span className="grey">Surf</span>
+                    <span className="grey">Algo</span> Surf <span style={{color: primary}}>{network}</span> 
                 </div>
                 <div className="search-section">
                     <Search autoFocus={true}></Search>
