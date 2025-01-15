70f552d557281e117467b73950d081c73dd5b042
Bit D13 <bit@d13.co>
2025-01-15 15:38:39 +0100

    localnet: retry when erroring

--- src/components/Modules/Explorer/Common/CustomError/CustomError.tsx
+++ src/components/Modules/Explorer/Common/CustomError/CustomError.tsx
@@ -1,5 +1,6 @@
 import './CustomError.scss';
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import {Button, Grid, Typography} from "@mui/material";
 import {Alert} from "@mui/lab";
 import {showSettings} from "../../../../../redux/settings/actions/settings";
@@ -8,9 +9,44 @@ import {useNavigate} from "react-router-dom";
 import {supportSettings} from "../../../../../utils/nodeConfig";
 import {shadedClr} from "../../../../../utils/common";
 
+const network = process.env.REACT_APP_NETWORK;
+const isLocalnet =  process.env.REACT_APP_NETWORK === "Localnet";
+
+const TOTAL_RETRIES = 5;
+
+function getRetries(hash: string): number {
+    const regex = /retry=(\d)/g
+    const match = regex.exec(hash);
+    if (match) {
+        return parseInt(match[1], 10);
+    }
+    return 0;
+}
+
 function CustomError({ error }: { error?: string }): JSX.Element {
+    const location = useLocation();
     const dispatch = useDispatch();
     const navigate = useNavigate();
+    const [retry, setRetry] = useState(0);
+    const [countdown, setCountdown] = useState(2);
+
+    useEffect(() => {
+        console.log('hash', location.hash);
+        if (isLocalnet) {
+            const retry = getRetries(location.hash);
+            setRetry(retry);
+            if (retry < TOTAL_RETRIES) {
+                const newLocation = new URL(window.location.href);
+                newLocation.hash = `#retry=${retry+1}`;
+                setTimeout(() => setCountdown(1), 1_000);
+                setTimeout(() => {
+                    setCountdown(0);
+                    window.history.replaceState(null, null, newLocation.toString());
+                    window.location.reload();
+                }, 2_000);
+            }
+        }
+    }, [location.hash]);
 
     return (<div className={"custom-error-wrapper"}>
         <div className={"custom-error-container"}>
@@ -23,6 +59,12 @@ function CustomError({ error }: { error?: string }): JSX.Element {
                         <Alert icon={false} sx={{backgroundColor: shadedClr, color: "white"}} style={{marginTop: 20, borderRadius: 10}}>
                             The resource you are looking for is not available.
                         </Alert>
+                        { isLocalnet ? <>
+                        <Alert icon={false} sx={{backgroundColor: shadedClr, color: "white"}} style={{marginTop: 20, borderRadius: 10}}>
+                            Localnet retry {retry} / {TOTAL_RETRIES} &middot;{' '}
+                            { retry < TOTAL_RETRIES ? <>Retrying in {countdown} seconds</> : <>Retries exhausted</> }
+                        </Alert>
+                        </> : null }
                     </div>
 
                     <div className="actions">
