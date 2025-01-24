8434d9a78e793fff794dd06524f572e6e32eacd4
Bit D13 <bit@d13.co>
2025-01-24 12:41:10 +0100

    Additional details link to block

--- src/components/Modules/Explorer/Records/Transaction/Sections/TransactionAdditionalDetails/TransactionAdditionalDetails.tsx
+++ src/components/Modules/Explorer/Records/Transaction/Sections/TransactionAdditionalDetails/TransactionAdditionalDetails.tsx
@@ -1,95 +1,99 @@
-import './TransactionAdditionalDetails.scss';
+import "./TransactionAdditionalDetails.scss";
 import React from "react";
-import {Accordion, AccordionDetails, AccordionSummary, Grid, Typography} from "@mui/material";
-import {ExpandMore} from "@mui/icons-material";
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Grid,
+  Typography,
+} from "@mui/material";
+import { ExpandMore } from "@mui/icons-material";
 import NumberFormat from "react-number-format";
 import AlgoIcon from "../../../../AlgoIcon/AlgoIcon";
-import {CoreTransaction} from "../../../../../../../packages/core-sdk/classes/core/CoreTransaction";
-import {shadedClr,microalgosToAlgos} from "../../../../../../../utils/common";
-
+import { CoreTransaction } from "../../../../../../../packages/core-sdk/classes/core/CoreTransaction";
+import {
+  shadedClr,
+  microalgosToAlgos,
+} from "../../../../../../../utils/common";
+import LinkToBlock from "../../../../Common/Links/LinkToBlock";
 
 function TransactionAdditionalDetails(props): JSX.Element {
+  const { transaction } = props;
+  const txnInstance = new CoreTransaction(transaction);
 
-    const {transaction} = props;
-    const txnInstance = new CoreTransaction(transaction);
+  return (
+    <div className={"transaction-additional-details-wrapper"}>
+      <div className={"transaction-additional-details-container"}>
+        <div
+          className="props"
+          style={{ background: shadedClr, padding: "10px" }}
+        >
+          <Accordion className="transparent rounded">
+            <AccordionSummary
+              expandIcon={<ExpandMore />}
+              id="additional-information"
+            >
+              <Typography>Additional information</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm={6} md={6} lg={3} xl={3}>
+                  <div className="property">
+                    <div className="key">First round</div>
+                    <div className="value">
+                      <LinkToBlock id={txnInstance.getFirstRound()} />
+                    </div>
+                  </div>
+                </Grid>
+                <Grid item xs={12} sm={6} md={6} lg={3} xl={3}>
+                  <div className="property">
+                    <div className="key">Last round</div>
+                    <div className="value">
+                      <LinkToBlock id={txnInstance.getLastRound()} />
+                    </div>
+                  </div>
+                </Grid>
 
-    return (<div className={"transaction-additional-details-wrapper"}>
-        <div className={"transaction-additional-details-container"}>
+                <Grid item xs={12} sm={6} md={6} lg={3} xl={3}>
+                  <div className="property">
+                    <div className="key">Sender rewards</div>
+                    <div className="value">
+                      <NumberFormat
+                        value={microalgosToAlgos(
+                          txnInstance.getSenderRewards()
+                        )}
+                        displayType={"text"}
+                        thousandSeparator={true}
+                        style={{ marginRight: 5 }}
+                      ></NumberFormat>
+                      <AlgoIcon></AlgoIcon>
+                    </div>
+                  </div>
+                </Grid>
 
-            <div className="props" style={{background: shadedClr, padding: '10px'}}>
-                <Accordion className="transparent rounded">
-                    <AccordionSummary
-                        expandIcon={<ExpandMore />}
-                        id="additional-information"
-                    >
-                        <Typography>Additional information</Typography>
-                    </AccordionSummary>
-                    <AccordionDetails>
-
-                        <Grid container spacing={2}>
-                            <Grid item xs={12} sm={6} md={6} lg={3} xl={3}>
-                                <div className="property">
-                                    <div className="key">
-                                        First round
-                                    </div>
-                                    <div className="value">
-                                        {txnInstance.getFirstRound()}
-                                    </div>
-                                </div>
-                            </Grid>
-                            <Grid item xs={12} sm={6} md={6} lg={3} xl={3}>
-                                <div className="property">
-                                    <div className="key">
-                                        Last round
-                                    </div>
-                                    <div className="value">
-                                        {txnInstance.getLastRound()}
-                                    </div>
-                                </div>
-                            </Grid>
-
-                            <Grid item xs={12} sm={6} md={6} lg={3} xl={3}>
-                                <div className="property">
-                                    <div className="key">
-                                        Sender rewards
-                                    </div>
-                                    <div className="value">
-                                        <NumberFormat
-                                            value={microalgosToAlgos(txnInstance.getSenderRewards())}
-                                            displayType={'text'}
-                                            thousandSeparator={true}
-                                            style={{marginRight: 5}}
-                                        ></NumberFormat>
-                                        <AlgoIcon></AlgoIcon>
-                                    </div>
-                                </div>
-                            </Grid>
-
-                            <Grid item xs={12} sm={6} md={6} lg={3} xl={3}>
-                                <div className="property">
-                                    <div className="key">
-                                        Receiver rewards
-                                    </div>
-                                    <div className="value">
-                                        <NumberFormat
-                                            value={microalgosToAlgos(txnInstance.getReceiverRewards())}
-                                            displayType={'text'}
-                                            thousandSeparator={true}
-                                            style={{marginRight: 5}}
-                                        ></NumberFormat>
-                                        <AlgoIcon></AlgoIcon>
-                                    </div>
-                                </div>
-                            </Grid>
-                        </Grid>
-
-                    </AccordionDetails>
-                </Accordion>
-            </div>
-
-
+                <Grid item xs={12} sm={6} md={6} lg={3} xl={3}>
+                  <div className="property">
+                    <div className="key">Receiver rewards</div>
+                    <div className="value">
+                      <NumberFormat
+                        value={microalgosToAlgos(
+                          txnInstance.getReceiverRewards()
+                        )}
+                        displayType={"text"}
+                        thousandSeparator={true}
+                        style={{ marginRight: 5 }}
+                      ></NumberFormat>
+                      <AlgoIcon></AlgoIcon>
+                    </div>
+                  </div>
+                </Grid>
+              </Grid>
+            </AccordionDetails>
+          </Accordion>
         </div>
-    </div>);
+      </div>
+    </div>
+  );
 }
 
 export default TransactionAdditionalDetails;
