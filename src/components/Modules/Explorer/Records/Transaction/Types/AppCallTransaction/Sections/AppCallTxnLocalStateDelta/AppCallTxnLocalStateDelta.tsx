4a3e40cc08550116689bc7375ba91eba9302becf
Bit D13 <bit@d13.co>
2025-03-14 23:11:20 +0200

    app call local state delta styling fix

--- src/components/Modules/Explorer/Records/Transaction/Types/AppCallTransaction/Sections/AppCallTxnLocalStateDelta/AppCallTxnLocalStateDelta.tsx
+++ src/components/Modules/Explorer/Records/Transaction/Types/AppCallTransaction/Sections/AppCallTxnLocalStateDelta/AppCallTxnLocalStateDelta.tsx
@@ -1,135 +1,130 @@
-import './AppCallTxnLocalStateDelta.scss';
+import "./AppCallTxnLocalStateDelta.scss";
 import React from "react";
-import {DataGrid, GridColDef, GridValueGetterParams} from "@mui/x-data-grid";
+import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
 import {
-    dataGridCellConfig,
-    dataGridStylesBlackHeader
+  dataGridCellConfig,
+  dataGridStyles,
+  dataGridStylesBlackHeader,
 } from "../../../../../../../../../theme/styles/datagrid";
 import NumberFormat from "react-number-format";
-import {A_LocalStateDelta} from "../../../../../../../../../packages/core-sdk/types";
-import {CoreLocalState} from "../../../../../../../../../packages/core-sdk/classes/core/CoreLocalStateDelta";
-import {Grid} from "@mui/material";
+import { A_LocalStateDelta } from "../../../../../../../../../packages/core-sdk/types";
+import { CoreLocalState } from "../../../../../../../../../packages/core-sdk/classes/core/CoreLocalStateDelta";
+import { Grid } from "@mui/material";
 import LinkToAccount from "../../../../../../Common/Links/LinkToAccount";
-import {shadedClr} from "../../../../../../../../../utils/common";
+import { shadedClr } from "../../../../../../../../../utils/common";
 
 function AppCallTxnLocalStateDelta(props): JSX.Element {
+  let localStateDelta: A_LocalStateDelta[] = props.state;
+  if (!localStateDelta) {
+    localStateDelta = [];
+  }
 
-    let localStateDelta: A_LocalStateDelta[] = props.state;
-    if (!localStateDelta) {
-        localStateDelta = [];
-    }
+  const columns: GridColDef[] = [
+    {
+      ...dataGridCellConfig,
+      field: "action",
+      headerName: "Action",
+      renderCell: (params: GridValueGetterParams) => {
+        const gStateDeltaInstance = new CoreLocalState(params.row);
+        return <div>{gStateDeltaInstance.getActionDisplayValue()}</div>;
+      },
+    },
+    {
+      ...dataGridCellConfig,
+      field: "type",
+      headerName: "Type",
+      renderCell: (params: GridValueGetterParams) => {
+        const gStateDeltaInstance = new CoreLocalState(params.row);
+        return <div>{gStateDeltaInstance.getType()}</div>;
+      },
+    },
+    {
+      ...dataGridCellConfig,
+      field: "key",
+      headerName: "Key",
+      renderCell: (params: GridValueGetterParams) => {
+        const gStateDeltaInstance = new CoreLocalState(params.row);
+        return <div>{gStateDeltaInstance.getKey()}</div>;
+      },
+    },
+    {
+      ...dataGridCellConfig,
+      field: "value",
+      headerName: "Value",
+      flex: 3,
+      renderCell: (params: GridValueGetterParams) => {
+        const gStateDeltaInstance = new CoreLocalState(params.row);
+        const action = gStateDeltaInstance.getAction();
+        return (
+          <div>
+            {action === 2 ? (
+              <NumberFormat
+                value={gStateDeltaInstance.getValue()}
+                displayType={"text"}
+                thousandSeparator={true}
+              ></NumberFormat>
+            ) : (
+              gStateDeltaInstance.getValue()
+            )}
+          </div>
+        );
+      },
+    },
+  ];
 
-    const columns: GridColDef[] = [
-        {
-            ...dataGridCellConfig,
-            field: 'key',
-            headerName: 'Key',
-            renderCell: (params: GridValueGetterParams) => {
-                const gStateDeltaInstance = new CoreLocalState(params.row);
-                return <div>
-                    {gStateDeltaInstance.getKey()}
-                </div>;
-            }
-        },
-        {
-            ...dataGridCellConfig,
-            field: 'type',
-            headerName: 'Type',
-            renderCell: (params: GridValueGetterParams) => {
-                const gStateDeltaInstance = new CoreLocalState(params.row);
-                return <div>
-                    {gStateDeltaInstance.getType()}
-                </div>;
-            }
-        },
-        {
-            ...dataGridCellConfig,
-            field: 'action',
-            headerName: 'Action',
-            renderCell: (params: GridValueGetterParams) => {
-                const gStateDeltaInstance = new CoreLocalState(params.row);
-                return <div>
-                    {gStateDeltaInstance.getActionDisplayValue()}
-                </div>;
-            }
-        },
-        {
-            ...dataGridCellConfig,
-            field: 'value',
-            headerName: 'Value',
-            flex: 2,
-            renderCell: (params: GridValueGetterParams) => {
-                const gStateDeltaInstance = new CoreLocalState(params.row);
-                const action = gStateDeltaInstance.getAction();
-                return <div>
-                    {action === 2 ? <NumberFormat
-                        value={gStateDeltaInstance.getValue()}
-                        displayType={'text'}
-                        thousandSeparator={true}
-                    ></NumberFormat> : gStateDeltaInstance.getValue()}
-                </div>;
-            }
-        }
-    ];
-
-    return (<div className={"app-call-txn-local-state-delta-wrapper"}>
-        <div className={"app-call-txn-local-state-delta-container"}>
-
-            <div className="props" style={{background: shadedClr}}>
-                <Grid container spacing={2}>
-                    <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-                        <div className="property">
-                            <div className="key" style={{paddingLeft: "6px"}}>
-                                Local state delta
+  return (
+    <div className={"app-call-txn-local-state-delta-wrapper"}>
+      <div className={"app-call-txn-local-state-delta-container"}>
+        <div className="props" style={{ background: shadedClr }}>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+              <div className="property">
+                <div className="key" style={{ paddingLeft: "6px" }}>
+                  Local state delta
+                </div>
+                <div className="value">
+                  <div className="app-call-txn-local-state-delta-body">
+                    {localStateDelta.map((accountLocalState) => {
+                      return (
+                        <div
+                          className="local-state-account-wrapper"
+                          key={accountLocalState.address}
+                        >
+                          <div className="local-state-account-container">
+                            <div className="address">
+                              <LinkToAccount
+                                copySize="m"
+                                address={accountLocalState.address}
+                              ></LinkToAccount>
                             </div>
-                            <div className="value" style={{marginTop: 20}}>
-
-                                <div className="app-call-txn-local-state-delta-body">
-
-                                    {localStateDelta.map((accountLocalState) => {
-                                        return <div className="local-state-account-wrapper" key={accountLocalState.address}>
-                                            <div className="local-state-account-container">
-                                                <div className="address">
-                                                    <LinkToAccount copySize="m" address={accountLocalState.address}></LinkToAccount>
-                                                </div>
-                                                <div className="state-delta">
-                                                    <div style={{ width: '100%' }}>
-                                                        <DataGrid
-                                                            rows={accountLocalState.delta}
-                                                            columns={columns}
-                                                            rowsPerPageOptions={[]}
-                                                            disableSelectionOnClick
-                                                            autoHeight
-                                                            sx={{
-                                                                ...dataGridStylesBlackHeader
-                                                            }}
-                                                            getRowId={(row) => {
-                                                                return row.key;
-                                                            }}
-                                                        />
-                                                    </div>
-
-                                                </div>
-                                            </div>
-                                        </div>;
-                                    })}
-
-
-
-                                </div>
-
-
+                            <div className="state-delta">
+                              <div style={{ width: "100%" }}>
+                                <DataGrid
+                                  rows={accountLocalState.delta}
+                                  columns={columns}
+                                  rowsPerPageOptions={[]}
+                                  disableSelectionOnClick
+                                  autoHeight
+                                  sx={dataGridStyles}
+                                  getRowId={(row) => {
+                                    return row.key;
+                                  }}
+                                />
+                              </div>
                             </div>
+                          </div>
                         </div>
-                    </Grid>
-                </Grid>
-            </div>
-
-
-
-
+                      );
+                    })}
+                  </div>
+                </div>
+              </div>
+            </Grid>
+          </Grid>
         </div>
-    </div>);
+      </div>
+    </div>
+  );
 }
 
 export default AppCallTxnLocalStateDelta;
