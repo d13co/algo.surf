6370a308b6e09a164b308aa58b66aec5127ef859
Bit D13 <bit@d13.co>
2025-12-11 11:48:41 +0100

    LinkToApplication copy button

--- src/components/Modules/Explorer/Common/Links/LinkToApplication.tsx
+++ src/components/Modules/Explorer/Common/Links/LinkToApplication.tsx
@@ -1,12 +1,18 @@
 import Link from './Link';
 import React from "react";
+import Copyable from "../../../../Common/Copyable/Copyable";
+
 import './LinkTo.scss';
 
-function LinkToApplication({id, className = "", children = undefined, name = ''}): JSX.Element {
+function LinkToApplication({id, className = "", children = undefined, copy = "right", copySize = "m", name = ''}): JSX.Element {
     if (!name) {
         name = id;
     }
-    return <Link className={`long-id ${className}`} href={"/application/" + id}>{children ?? name}</Link>;
+    return <>
+        <Link className={`long-id ${className}`} href={"/application/" + id}>{children ?? name}</Link>
+        {copy === "right" ? <Copyable size={copySize as any} value={id} /> : null}
+
+    </>
 }
 
 export default LinkToApplication;
