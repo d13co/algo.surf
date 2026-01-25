505ed3691878b768f8fba1e48ed340ffd19d1225
Bit D13 <bit@d13.co>
2026-01-26 00:43:54 +0100

    order boxes by ascii value

--- src/packages/core-sdk/clients/boxClient.ts
+++ src/packages/core-sdk/clients/boxClient.ts
@@ -1,27 +1,31 @@
-import {Algodv2} from 'algosdk';
+import { Algodv2 } from "algosdk";
 import type { Indexer } from "algosdk";
-import {A_BoxName, A_BoxNames, A_Box,} from "../types";
-import {Network} from "../network";
+import { A_BoxNames, A_Box } from "../types";
+import { Network } from "../network";
 
-export class BoxClient{
-    client: Algodv2;
-    indexer: Indexer;
-    network: Network
+export class BoxClient {
+  client: Algodv2;
+  indexer: Indexer;
+  network: Network;
 
-    constructor(network: Network) {
-        this.network = network;
-        this.client = network.getClient();
-        this.indexer = network.getIndexer();
-    }
+  constructor(network: Network) {
+    this.network = network;
+    this.client = network.getClient();
+    this.indexer = network.getIndexer();
+  }
 
-    async getBoxNames(id: number): Promise<A_BoxNames> {
-        const app = await this.client.getApplicationBoxes(id).do();
-        const bufferNames = app.boxes.map(({ name }) => ({ name: Buffer.from(name).toString('base64') }));
-        return bufferNames;
-    }
+  async getBoxNames(id: number): Promise<A_BoxNames> {
+    const app = await this.client.getApplicationBoxes(id).do();
+    const bufferNames = app.boxes
+      .sort(({ name: a }, { name: b }) => Buffer.compare(a, b))
+      .map(({ name }) => ({ name: Buffer.from(name).toString("base64") }));
+    return bufferNames;
+  }
 
-    async getBox(id: number, name: string): Promise<A_Box> {
-        const { value } = await this.client.getApplicationBoxByName(id, Buffer.from(name, 'base64')).do();
-        return { name, value: Buffer.from(value).toString("base64"), };
-    }
+  async getBox(id: number, name: string): Promise<A_Box> {
+    const { value } = await this.client
+      .getApplicationBoxByName(id, new Uint8Array(Buffer.from(name, "base64")))
+      .do();
+    return { name, value: Buffer.from(value).toString("base64") };
+  }
 }
