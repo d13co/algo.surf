59e346af4cdba9acfaa026433af32fd8a5564a07
Bit D13 <bit@d13.co>
2026-03-23 23:05:55 +0100

    delete dead files

--- src/components/Modules/Explorer/Lists/BoxesList/BoxesList.tsx
+++
@@ -1,1 +0,0 @@
-export { default } from "src/components/Modules/Explorer/v2/BoxesList";
--- src/packages/arc-portal/types.ts
+++ src/packages/arc-portal/types.ts
@@ -1,16 +1,5 @@
-export type A_Arc = {
-    id: number
-    name: string
-    markdownUrl: string
-    markdown: string
-    github: string,
-    hasWorkspace: boolean
-};
-
 export type A_Arc_Validation = {
     valid: boolean,
     errors: string[],
     warnings: string[],
 };
-
-// Pruned non-ARC19 metadata types
